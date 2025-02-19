36b37d0e7153a7c5fb704a703d8ad9f61687fb82
Tanvir Hridoy <167385020+IkramulHasanTanvir@users.noreply.github.com>
2025-02-19 19:13:39 +0600

    ....

--- lib/app/binding/app_initial_binding.dart
+++ lib/app/binding/app_initial_binding.dart
@@ -1,6 +1,8 @@
 import 'package:crafty_bay/features/auth_pages/controller/login_controller.dart';
 import 'package:crafty_bay/features/auth_pages/controller/otp_verify_controller.dart';
 import 'package:crafty_bay/features/auth_pages/controller/sing_up_controller.dart';
+import 'package:crafty_bay/features/bottom_nav/controller/bottom_nav_controller.dart';
+import 'package:crafty_bay/features/home/controller/slider_list_controller.dart';
 import 'package:get/get.dart';
 
 class AppInitialBinding extends Bindings{
@@ -9,6 +11,8 @@ class AppInitialBinding extends Bindings{
     Get.put(LoginController());
     Get.put(SingUpController());
     Get.put(OtpVerifyController());
+    Get.put(BottomNavController());
+    Get.put(SliderListController());
   }
 
 }
--- lib/routes/route_pages.dart
+++ lib/routes/route_pages.dart
@@ -3,6 +3,7 @@ import 'package:crafty_bay/features/auth_pages/views/forgot_email_screen.dart';
 import 'package:crafty_bay/features/auth_pages/views/login_screen.dart';
 import 'package:crafty_bay/features/auth_pages/views/otp_screen.dart';
 import 'package:crafty_bay/features/auth_pages/views/signup_screen.dart';
+import 'package:crafty_bay/features/bottom_nav/views/bottom_nav_screen.dart';
 import 'package:crafty_bay/features/into_pages/views/spalsh_screen.dart';
 import 'package:crafty_bay/routes/route_name.dart';
 import 'package:flutter/material.dart';
@@ -26,7 +27,7 @@ class RoutePages {
     } else if (settings.name == RouteName.changePassScreen) {
       widget = const ChangePassScreen();
     } else if (settings.name == RouteName.mainBottomNav) {
-      widget = const SignupScreen();
+      widget = const BottomNavScreen();
     }
     return MaterialPageRoute(
       builder: (ctx) {
